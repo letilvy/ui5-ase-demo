281a1994a2cb193c363848875e1b3f4ad5ad45c1
Xiaocheng Wei <xiaocheng.wei@sap.com>
2016-12-22 12:57:14 +0800

    Rename WebContent to webapp and add sinon unit test case

---
+++ webapp/model/models.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	'sap/ui/model/json/JSONModel',
+	'sap/ui/Device'
+], function (JSONModel, Device) {
+	"use strict";
+
+	return {
+		createDeviceModel: function () {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+		
+		errorHandler: function(iErrorCode){
+			 var sIcon = parseInt(iErrorCode)%2 === 0 ? "WARNING":"ERROR";
+			 
+			 MessageBox.show("Error Message Goes Here", {
+			 	icon: sIcon,
+				title: "My message box title",
+       		actions: [sap.m.MessageBox.Action.Cancel]
+			 });
+		}
+	};
+
+});
--- webapp/test/unit/model/models.js
+++ webapp/test/unit/model/models.js
@@ -59,4 +59,20 @@ sap.ui.require(
 			// Assert
 			assert.strictEqual(this.oDeviceModel.getDefaultBindingMode(), "OneWay", "Binding mode is correct");
 		});
+		
+		
+		QUnit.module("Sinon test");
+		
+		QUnit.test("Should show warning message box when error code is even", function(assert){
+			this.stub(MessageBox, "show");
+			models.errorHandler(2);
+			assert.strictEqual(MessageBox.show.getCall(0).args[1].icon, "WARNING", "Message box type is correct");
+		});
+		
+		QUnit.test("Should show warning message box when error code is odd", function(assert){
+			var oSpy = this.stub(MessageBox, "show");
+			models.errorHandler(3);
+			//assert.strictEqual(MessageBox.show.getCall(0).args[1].icon, "ERROR", "Message box type is correct");
+			assert.ok(oSpy.calledWith(sinon.match.string, sinon.match({icon:"ERROR"})), "Message box type is correct");
+		});
 	});
