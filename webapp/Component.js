618ab942a7e0b15076b0ad813815385796324f77
Qian Carol <c.qian@sap.com>
2017-09-08 05:56:59 +0000

    test from carol

--- webapp/Component.js
+++ webapp/Component.js
@@ -11,7 +11,7 @@ sap.ui.define([
 			metadata : {
 				manifest: "json"
 			},
-
+ 
 			/**
 			 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
 			 * In this function, the device models are set and the router is initialized.
