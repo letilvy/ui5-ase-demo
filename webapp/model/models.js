4e79378b54500b0ac78fb6b679dc86fec5de2b35
Xiaocheng Wei <xiaocheng.wei@sap.com>
2017-09-12 17:56:14 +0800

    remove sinon example code

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -10,16 +10,6 @@ sap.ui.define([
 			var oModel = new JSONModel(Device);
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
-		},
-		
-		errorHandler: function(iErrorCode){
-			 var sIcon = parseInt(iErrorCode)%2 === 0 ? "WARNING":"ERROR";
-			 
-			 MessageBox.show("Error Message Goes Here", {
-			 	icon: sIcon,
-				title: "My message box title",
-        		actions: [sap.m.MessageBox.Action.Cancel]
-			 });
 		}
 	};
 
