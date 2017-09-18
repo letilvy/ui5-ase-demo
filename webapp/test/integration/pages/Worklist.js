cbae5da6b3c9e8b1d76d692b84dea4323b06c8d6
Qian Carol <c.qian@sap.com>
2017-09-18 02:19:27 +0000

    v3.0

--- webapp/test/integration/pages/Worklist.js
+++ webapp/test/integration/pages/Worklist.js
@@ -2,48 +2,26 @@ sap.ui.require([
 		'sap/ui/test/Opa5',
 		'sap/ui/test/matchers/AggregationLengthEquals',
 		'sap/ui/test/matchers/PropertyStrictEquals',
-		'sap/ui/test/matchers/BindingPath',
-		'sap/ui/demo/bulletinboard/test/integration/pages/Common',
-		'sap/ui/test/actions/Press'
+		'sap/ui/demo/bulletinboard/test/integration/pages/Common'
 	],
 	function(Opa5,
 		AggregationLengthEquals,
 		PropertyStrictEquals,
-		BindingPath,
-		Common,
-		Press) {
+		Common) {
 		"use strict";
 
-		var sViewName = "Worklist",
-			sTableId = "table";
+		var sViewName = "Worklist";
 
 		Opa5.createPageObjects({
 			onTheWorklistPage: {
 				baseClass: Common,
 				actions: {
 					iPressOnTheItemWithTheID: function(sId) {
-						return this.waitFor({
-							controlType: "sap.m.ColumnListItem",
-							viewName: sViewName,
-							matchers: new BindingPath({
-								path: "/Posts('" + sId + "')"
-							}),
-							actions: new Press(),
-							errorMessage: "No list item with the ID " + sId + " was found."
-						});
+						/* matchers: new BindingPath() */
 					}
 				},
 				assertions: {
-					iShouldSeeTheTable: function() {
-						return this.waitFor({
-							id: sTableId,
-							viewName: sViewName,
-							success: function() {
-								Opa5.assert.ok(true, "The table is visible");
-							},
-							errorMessage: "Was not able to see the table."
-						});
-					}
+					iShouldSeeTheTable: function() {}
 				}
 			}
 		});
