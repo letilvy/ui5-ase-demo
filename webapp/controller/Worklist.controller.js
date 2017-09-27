b6e47be3e1a350f6772ae6616cb396fabfc71780
Qian Carol <c.qian@sap.com>
2017-09-27 02:16:18 +0000

    v 1.3 remove Base Controller from Worklist.controller

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -1,38 +1,27 @@
 /*global history*/
 
-sap.ui.define([
-	'sap/ui/demo/bulletinboard/controller/BaseController',
-	'sap/ui/model/json/JSONModel',
-	'sap/ui/demo/bulletinboard/model/formatter',
-	'sap/ui/demo/bulletinboard/model/FlaggedType',
+sap.ui.define([ 
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/demo/bulletinboard/model/formatter",
+	"sap/ui/demo/bulletinboard/model/FlaggedType",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator"
-], function (BaseController, JSONModel, formatter, FlaggedType, Filter, FilterOperator) {
+], function(Controller, JSONModel, formatter, FlaggedType, Filter, FilterOperator) {
 	"use strict";
 
-	return BaseController.extend("sap.ui.demo.bulletinboard.controller.Worklist", {
-		types : {
+	return Controller.extend("sap.ui.demo.bulletinboard.controller.Worklist", {
+		types: {
 			flagged: new FlaggedType()
 		},
 
 		formatter: formatter,
 
-		/* =========================================================== */
-		/* lifecycle methods                                           */
-		/* =========================================================== */
-
-		/**
-		 * Called when the worklist controller is instantiated.
-		 * @public
-		 */
-		onInit: function () {
+		onInit: function() {
 			var oViewModel,
 				iOriginalBusyDelay,
 				oTable = this.byId("table");
 
-			// Put down worklist table's original value for busy indicator delay,
-			// so it can be restored later on. Busy handling on the table is
-			// taken care of by the table itself.
 			iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
 
 			// Model used to manipulate control states
@@ -42,32 +31,15 @@ sap.ui.define([
 				shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [window.location.href]),
 				tableBusyDelay: 0
 			});
-			this.setModel(oViewModel, "worklistView");
+			this.getView().setModel(oViewModel, "worklistView");
 
-			// Make sure, busy indication is showing immediately so there is no
-			// break after the busy indication for loading the view's meta data is
-			// ended (see promise 'oWhenMetadataIsLoaded' in AppController)
-			oTable.attachEventOnce("updateFinished", function () {
+			oTable.attachEventOnce("updateFinished", function() {
 				// Restore original busy indicator delay for worklist's table
 				oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
 			});
 		},
 
-		/* =========================================================== */
-		/* event handlers                                              */
-		/* =========================================================== */
-
-		/**
-		 * Triggered by the table's 'updateFinished' event: after new table
-		 * data is available, this handler method updates the table counter.
-		 * This should only happen if the update was successful, which is
-		 * why this handler is attached to 'updateFinished' and not to the
-		 * table's list binding's 'dataReceived' method.
-		 *
-		 * @param {sap.ui.base.Event} oEvent the update finished event
-		 * @public
-		 */
-		onUpdateFinished: function (oEvent) {
+		onUpdateFinished: function(oEvent) {
 			// update the worklist's object counter after the table update
 			var sTitle,
 				oTable = oEvent.getSource(),
@@ -79,15 +51,10 @@ sap.ui.define([
 			} else {
 				sTitle = this.getResourceBundle().getText("worklistTableTitle");
 			}
-			this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
+			this.getView().getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
 		},
 
-		/**
-		 * Triggered by the SearchFields's 'search' event
-		 * @param {sap.ui.base.Event} oEvent SearchFields's search event
-		 * @public
-		 */
-		onFilterPosts: function (oEvent) {
+		onFilterPosts: function(oEvent) {
 
 			// build filter array
 			var aFilter = [];
@@ -102,29 +69,15 @@ sap.ui.define([
 			oBinding.filter(aFilter);
 		},
 
-		/**
-		 * Event handler when a table item gets pressed
-		 * @param {sap.ui.base.Event} oEvent the table selectionChange event
-		 * @public
-		 */
-		onPress: function (oEvent) {
-			this.getRouter().navTo("post", {
+		onPress: function(oEvent) {
+			sap.ui.core.UIComponent.getRouterFor(this).navTo("post", {
 				// The source is the list item that got pressed
 				postId: oEvent.getSource().getBindingContext().getProperty("PostID")
 			});
 
 		},
 
-		/* =========================================================== */
-		/* internal methods                                            */
-		/* =========================================================== */
-
-		/**
-		 * Sets the item count on the worklist view header
-		 * @param {int} iTotalItems the total number of items in the table
-		 * @private
-		 */
-		_updateListItemCount: function (iTotalItems) {
+		_updateListItemCount: function(iTotalItems) {
 			var sTitle;
 			// only update the counter if the length is final
 			if (this._oTable.getBinding("items").isLengthFinal()) {
@@ -133,18 +86,19 @@ sap.ui.define([
 			}
 		},
 
-		/**
-		 * Event handler when the share by E-Mail button has been clicked
-		 * @public
-		 */
-		onShareEmailPress: function () {
-			var oViewModel = this.getModel("worklistView");
+		onShareEmailPress: function() {
+			var oViewModel = this.getView().getModel("worklistView");
 			sap.m.URLHelper.triggerEmail(
 				null,
 				oViewModel.getProperty("/shareSendEmailSubject"),
 				oViewModel.getProperty("/shareSendEmailMessage")
 			);
-		}
+		},
+
+		getResourceBundle: function() {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		} 
+
 	});
 
 });
