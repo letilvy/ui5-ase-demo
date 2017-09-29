23e9cda997e0a30def7dc816ba68642f2770bbfb
Qian Carol <c.qian@sap.com>
2017-09-29 08:03:05 +0000

    v 2.0 remove search field

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -80,27 +80,7 @@ sap.ui.define([
 				sTitle = this.getResourceBundle().getText("worklistTableTitle");
 			}
 			this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
-		},
-
-		/**
-		 * Triggered by the SearchFields's 'search' event
-		 * @param {sap.ui.base.Event} oEvent SearchFields's search event
-		 * @public
-		 */
-		onFilterPosts: function (oEvent) {
-
-			// build filter array
-			var aFilter = [];
-			var sQuery = oEvent.getParameter("query");
-			if (sQuery) {
-				aFilter.push(new Filter("Title", FilterOperator.Contains, sQuery));
-			}
-
-			// filter binding
-			var oTable = this.getView().byId("table");
-			var oBinding = oTable.getBinding("items");
-			oBinding.filter(aFilter);
-		},
+		}, 
 
 		/**
 		 * Event handler when a table item gets pressed
