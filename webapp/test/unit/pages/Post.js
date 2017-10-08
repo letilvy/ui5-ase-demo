d650e0bb1f007566aa74e999b1f51c51439bf733
Sierra Yang <sierra.yang@sap.com>
2017-10-08 04:19:03 +0000

    remove synchronization

--- webapp/test/unit/pages/Post.js
+++ webapp/test/unit/pages/Post.js
@@ -1,17 +1,18 @@
 sap.ui.require(
 	[
 		"sap/ui/demo/bulletinboard/controller/Post.controller",
+		"sap/ui/core/mvc/View",
 		"sap/ui/model/odata/v2/ODataModel",
 		"sap/ui/thirdparty/sinon",
 		"sap/ui/thirdparty/sinon-qunit"
 	],
-	function (Post, ODataModel) {
+	function (Post, View, ODataModel) {
 		"use strict";
 		
 		QUnit.module("add and remove favorite",{
 		    beforeEach: function() {
     			this.post = new Post();
-    			this.viewStub = new sap.ui.core.mvc.View({});
+    			this.viewStub = new View({});
 			    sinon.stub(this.post, "getView").returns(this.viewStub);
 			    this.viewStub.setModel(new ODataModel(""));    
     		},
