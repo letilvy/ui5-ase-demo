8d62c07094b4b0f06926744e40b7f88d1a520040
Qian Carol <c.qian@sap.com>
2017-10-09 08:04:30 +0000

    Upgrade QUnit version to QUnit 2.0 for all of our exercise. TODO:  still need to replace

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,11 +1,11 @@
 /*global QUnit*/
 
-jQuery.sap.require("sap.ui.qunit.qunit-css");
 jQuery.sap.require("sap.ui.thirdparty.qunit");
+jQuery.sap.require("sap.ui.qunit.qunit-2-css");
 jQuery.sap.require("sap.ui.qunit.qunit-junit");
 QUnit.config.autostart = false;
 
-
+ 
 sap.ui.require([
 	"sap/ui/test/Opa5",
 	"sap/ui/demo/bulletinboard/test/integration/pages/Common",
@@ -14,7 +14,7 @@ sap.ui.require([
 	"sap/ui/demo/bulletinboard/test/integration/pages/Post"
 ], function (Opa5, Common) {
 	"use strict";
-	Opa5.extendConfig({
+	Opa5.extendConfig({ 
 		arrangements: new Common(),
 		viewNamespace: "sap.ui.demo.bulletinboard.view.",
 		autoWait: true,
