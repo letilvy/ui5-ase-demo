878a931d9dbdbfd87672261c08244bdfc9777ae4
Richard Gong <gongminglei@hotmail.com>
2018-06-26 15:17:57 +0800

    Update Greeter.js

--- webapp/test/unit/model/Greeter.js
+++ webapp/test/unit/model/Greeter.js
@@ -6,21 +6,21 @@ sap.ui.require([
   'use strict';
   QUnit.module("Greeter");
 
-  QUnit.test("Should only return greeting string if no name is given", function (assert) {
+  QUnit.test("Should only return greeting string when no name is given", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 14, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet(), "Hello");
   });
 
-  QUnit.test("Should only return greeting string if name is empty string", function (assert) {
+  QUnit.test("Should only return greeting string when name is empty string", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 14, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet(''), "Hello");
   });
 
-  QUnit.test("Should only return greeting string if name only contains space", function (assert) {
+  QUnit.test("Should only return greeting string when name only contains space", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 14, 0, 0, 0).getTime();;
     });
@@ -34,70 +34,70 @@ sap.ui.require([
     assert.strictEqual(oGreeter.greet(' Richard '), "Hello Richard");
   });
 
-  QUnit.test("Should return Good morning if time is 6:00", function (assert) {
+  QUnit.test("Should return Good morning when time is 6:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 6, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Good morning Richard");
   });
 
-  QUnit.test("Should return Good morning if time is 8:00", function (assert) {
+  QUnit.test("Should return Good morning when time is 8:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 8, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Good morning Richard");
   });
 
-  QUnit.test("Should return Hello if time is 12:00", function (assert) {
+  QUnit.test("Should return Hello when time is 12:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 12, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Hello Richard");
   });
 
-  QUnit.test("Should return Hello if time is 14:00", function (assert) {
+  QUnit.test("Should return Hello when time is 14:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 14, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Hello Richard");
   });
 
-  QUnit.test("Should return Good evening if time is 18:00", function (assert) {
+  QUnit.test("Should return Good evening when time is 18:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 18, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Good evening Richard");
   });
 
-  QUnit.test("Should return Good evening if time is 20:00", function (assert) {
+  QUnit.test("Should return Good evening when time is 20:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 20, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Good evening Richard");
   });
 
-  QUnit.test("Should return Good night if time is 22:00", function (assert) {
+  QUnit.test("Should return Good night when time is 22:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 22, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Good night Richard");
   });
 
-  QUnit.test("Should return Good night if time is 23:59", function (assert) {
+  QUnit.test("Should return Good night when time is 23:59", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 23, 59, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Good night Richard");
   });
 
-  QUnit.test("Should return Good night if time is 01:00", function (assert) {
+  QUnit.test("Should return Good night when time is 01:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 1, 0, 0, 0).getTime();;
     });
     assert.strictEqual(oGreeter.greet('Richard'), "Good night Richard");
   });
 
-  QUnit.test("Should return Good night if time is 05:00", function (assert) {
+  QUnit.test("Should return Good night when time is 05:00", function (assert) {
     var oGreeter = new Greeter(function () {
       return new Date(2018, 6, 21, 5, 0, 0, 0).getTime();;
     });
