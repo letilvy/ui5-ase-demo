0e3a11462afcfea524142d4f54309549351d85d0
Richard Gong <gongminglei@hotmail.com>
2018-07-01 22:41:56 +0800

    Update formatter.js

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,20 +1,9 @@
 sap.ui.define([
-	"sap/ui/demo/bulletinboard/model/DateFormatter"
 ], function (DateFormatter) {
 	"use strict";
 
 	return {
 
-		/**
-		 * Creates a human readable date
-		 *
-		 * @public
-		 * @param {Date} oDate the date of the property.
-		 * @returns {string} sValue the formatted date
-		 */
-		date: function(oDate) {
-			return new DateFormatter({ now: Date.now }).format(oDate);
-		}
 };
 
 });
