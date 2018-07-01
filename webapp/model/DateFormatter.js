ecb1e8f47b814ba7eaaf8621d538bced9852c355
Richard Gong <gongminglei@hotmail.com>
2018-07-01 22:56:41 +0800

    Create DateFormatter.js

---
+++ webapp/model/DateFormatter.js
@@ -0,0 +1,13 @@
+sap.ui.define(["sap/ui/base/Object", "sap/ui/core/format/DateFormat"],
+	function(Object, DateFormat) {
+		"use strict";
+
+		return Object.extend("sap.ui.demo.bulletinboard.model.DateFormatter", {
+			constructor: function() {
+			},
+      
+			format: function(oDate) {
+          return "";
+			}
+		});
+	});
