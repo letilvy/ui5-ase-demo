c76eec4543ff1417076e62e84bd78c9cc1ce195f
Richard Gong <gongminglei@hotmail.com>
2018-07-01 23:00:51 +0800

    Create DateFormatter.js

---
+++ webapp/test/unit/model/DateFormatter.js
@@ -0,0 +1,12 @@
+/*global QUnit*/
+
+sap.ui.require(
+	[
+		"sap/ui/demo/bulletinboard/model/DateFormatter",
+		"sap/ui/core/Locale"
+	],
+	function(DateFormatter, Locale) {
+		"use strict";
+    
+	}
+);
