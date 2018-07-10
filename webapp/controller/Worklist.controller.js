79da13c7b2855341173854b385049875d992eda6
Qian Carol <c.qian@sap.com>
2018-07-10 06:14:46 +0000

    update

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
 	"sap/ui/demo/bulletinboard/model/FlaggedType",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator"
-], function(Controller, JSONModel, formatter, FlaggedType, Filter, FilterOperator) {
+], function (Controller, JSONModel, formatter, FlaggedType, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.bulletinboard.controller.Worklist", {
@@ -17,7 +17,7 @@ sap.ui.define([
 
 		formatter: formatter,
 
-		onInit: function() {
+		onInit: function () {
 			var oViewModel,
 				iOriginalBusyDelay,
 				oTable = this.byId("table");
@@ -33,34 +33,24 @@ sap.ui.define([
 			});
 			this.getView().setModel(oViewModel, "worklistView");
 
-			oTable.attachEventOnce("updateFinished", function() {
+			oTable.attachEventOnce("updateFinished", function () {
 				// Restore original busy indicator delay for worklist's table
 				oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
 			});
 		},
 
-		onUpdateFinished: function(oEvent) {
-			var sTitle;
-			/* Task: 
-					Add count of itmes to the title, e.g. Posts (23)
-					If no item in the worklist, then still display 'Posts' 
-					Now it only shows 'Posts' as worklist title
-				Hint:
-					oEvent already provided a parameter for count of items
-			*/
-			// this.getView().getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
-			// sTitle = this.getResourceBundle().getText("worklistTableTitle");
-		}, 
+		onUpdateFinished: function (oEvent) {
 
-		onPress: function(oEvent) {
+		},
+
+		onPress: function (oEvent) {
 			sap.ui.core.UIComponent.getRouterFor(this).navTo("post", {
-				// The source is the list item that got pressed
 				postId: oEvent.getSource().getBindingContext().getProperty("PostID")
 			});
 
 		},
 
-		onShareEmailPress: function() {
+		onShareEmailPress: function () {
 			var oViewModel = this.getView().getModel("worklistView");
 			sap.m.URLHelper.triggerEmail(
 				null,
@@ -69,7 +59,7 @@ sap.ui.define([
 			);
 		},
 
-		getResourceBundle: function() {
+		getResourceBundle: function () {
 			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
 		}
 
