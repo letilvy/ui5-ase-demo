c96c86021d2b5187124cb1993c99cc5ab4177c93
Sierra Yang <sierra.yang@sap.com>
2018-07-17 08:23:17 +0000

    add post controller in unit test

--- webapp/test/unit/allTests.js
+++ webapp/test/unit/allTests.js
@@ -1,5 +1,6 @@
 sap.ui.define([
     "test/unit/model/utils",
 	"test/unit/model/models",
-	"test/unit/model/formatter"
+	"test/unit/model/formatter",
+	"test/unit/pages/Post.controller"
 ], function() { "use strict"; });
