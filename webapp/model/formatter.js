f6838bd8acbb69a66c251663528c1f17f8191c09
Yang Xia <DownXia@users.noreply.github.com>
2018-07-17 13:34:24 +0800

    Add formatter method
    - numberUnit
    - priceState

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -4,7 +4,27 @@ sap.ui.define([
 	"use strict";
 
 	return {
+		/**
+		 * Rounds the number unit value to 2 digits
+		 *
+		 * @public
+		 * @param {string} sValue the number string to be rounded
+		 * @returns {string} sValue with 2 digits rounded
+		 */
+		numberUnit: function (sValue) {
 
+		},
+
+		/**
+		 * Defines a value state based on the price
+		 *
+		 * @public
+		 * @param {number} iPrice the price of a post
+		 * @returns {string} sValue the state for the price
+		 */
+		priceState: function (iPrice) {
+
+		}
 	};
 
 });
