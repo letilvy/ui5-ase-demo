f6f070423834d1b141e60b4799391aaf72486be5
Sierra Yang <sierra.yang@sap.com>
2019-08-23 11:21:37 +0800

    add frame in worklist test file

---
+++ webapp/test/unit/controller/Worklist.controller.js
@@ -0,0 +1,34 @@
+sap.ui.require([
+	"sap/ui/demo/bulletinboard/controller/Worklist.controller",
+	"sap/ui/model/resource/ResourceModel"
+], function (WorklistController, ResourceModel) {
+	"use strict";
+	QUnit.module("Worklist Controller", {
+		before: function () {
+			this.oI18n = new ResourceModel({
+				bundleName: "sap.ui.demo.bulletinboard.i18n.i18n",
+				bundleLocale: "en"
+			});
+		},
+		beforeEach: function () {
+			this.oController = new WorklistController();
+			sinon.stub(this.oController, "getResourceBundle").returns(this.oI18n.getResourceBundle());
+
+			this.oViewModel = new JSONModel({
+				worklistTableTitle: null
+			});
+			sinon.stub(this.oController, "getModel").withArgs("worklistView").returns(this.oViewModel);
+		},
+		afterEach: function () {
+			sinon.restore();
+			this.oViewModel.destroy();
+			this.oController.destroy();
+		},
+		after: function () {
+			this.oI18n.destroy();
+		}
+	});
+
+	QUnit.test("", function (assert) {
+
+	});
