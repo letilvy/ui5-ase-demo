27a1f065ddf7659fc1c7786f381e9996c1e87784
Xiaocheng Wei <xiaocheng.wei@sap.com>
2022-07-06 18:33:05 +0800

    Initialize Karma Running Solution

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,21 @@
 module.exports = function(config){
   config.set({
-    frameworks: ["ui5"],
-
+    frameworks: ["ui5", "qunit", "sinon"],
     ui5: {
       url: "https://openui5.hana.ondemand.com",
+      mode: "script",
+      config: {
+        async: true,
+        resourceRoots: {
+          "sap.ui.demo.bulletinboard": "./base/webapp",
+          "test": "./base/webapp/test"
+        }
+      },
+      tests: [
+        "sap/ui/demo/bulletinboard/test/unit/allTests"
+      ]
     },
-
-    browsers: ["Chrome"]
+    browsers: ["ChromeHeadless"],
+    singleRun: true
   });
 };
